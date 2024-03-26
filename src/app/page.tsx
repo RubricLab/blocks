c7e2d145eb62395cf3e06ca29ff766aed24cdeaa
DexterStorey <dexter@dexterstorey.com>
2024-03-26 14:07:12 -0400

    ii

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { db } from "~/utils/db"
+
 export default async function Page() {
-	return <div>genuine</div>
+	return <div>
+		<h1>genuine</h1>
+		{(await db.api.findMany({select: {name: true}})).map(api => <div key={api.name}> {api.name} </div>)}
+
+		<form action={
+			async (formData: FormData) => {
+				'use server'
+				const { name, baseUrl } = Object.fromEntries(formData) as {name: string, baseUrl: string}
+				console.log('submit')
+				await db.api.create({data: {name, baseUrl}})
+			}
+		}>
+			<input type='text' name='name' placeholder="name" />
+			<input type='text' name='baseUrl' placeholder="baseUrl" />
+			<button type='submit'>Submit</button>
+		</form>
+	</div>
 }
